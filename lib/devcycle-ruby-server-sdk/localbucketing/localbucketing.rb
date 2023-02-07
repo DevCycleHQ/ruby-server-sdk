626f2215054bdab4cf3387e054deb3b0b01b4656
Jamie Sinn <jamie.sinn@taplytics.com>
2023-02-07 12:01:04 -0500

    Got memory manipulation working properly.

--- lib/devcycle-ruby-server-sdk/localbucketing/localbucketing.rb
+++ lib/devcycle-ruby-server-sdk/localbucketing/localbucketing.rb
@@ -1,10 +1,9 @@
 require 'wasmtime'
 require 'date'
-
+require 'bigdecimal'
 # Temp commenting out the module for testing within the file raw.
 # module DevCycle
 class LocalBucketing
-
 
   @@rand = Random.new(seed = Random.new_seed)
   @@engine = Wasmtime::Engine.new
@@ -17,8 +16,6 @@ class LocalBucketing
   @@store = Wasmtime::Store.new(@@engine, wasi_ctx: @@wasi_ctx)
   @@linker = Wasmtime::Linker.new(@@engine, wasi: true)
 
-
-
   @@linker.func_new("env", "Date.now", [], [:f64]) do |_caller|
     # Return an array with 2 elements for 2 results
     Time.now.to_f
@@ -26,34 +23,54 @@ class LocalBucketing
 
   @@linker.func_new("env", "abort", [:i32, :i32, :i32, :i32], []) do |_caller, messagePtr, filenamePtr, lineNum, colNum|
 
-    @@memory.read_unsafe_slice(messagePtr)
-    raw_bytes = [@@memory.read_unsafe_slice(messagePtr - 1, 1).to_str,@@memory.read_unsafe_slice(messagePtr - 2, 1),@@memory.read_unsafe_slice(messagePtr - 3, 1),@@memory.read_unsafe_slice(messagePtr - 4, 1)]
-    puts(raw_bytes)
-    length = raw_bytes.unpack("N").first
-    result = ""
-    i = 0
-    while i < length
-      result += @@memory.read(messagePtr + i, 1)
-      i += 2
-    end
-    message = result
+    exceptionMessage = ""
+    exceptionFilename = ""
 
+    [messagePtr, filenamePtr].each { |m|
+      raw_bytes = @@memory.read(m - 4, 4).bytes.reverse
+      message_len = 0
+      raw_bytes.each { |j|
+        message_len = (message_len << 8) + (j & 0xFF)
+      }
+      length = message_len
+      result = ""
+      i = 0
+      while i < length
+        result += @@memory.read(m + i, 1)
+        i += 2
+      end
+      if m == messagePtr
+        exceptionMessage = result
+      else
+        if m == filenamePtr
+          exceptionFilename = result
+        end
+      end
+    }
 
-    length = @@memory.read(filenamePtr - 4, 4).unpack("N").first
-    result = ""
-    i = 0
-    while i < length
-      result += @@memory.read(filenamePtr + i, 1)
-      i += 2
-    end
-    filename = result
-    STDERR.puts("WASM Exception: #{message}@#{filename} #{lineNum}:#{colNum}")
+    STDERR.puts("WASM Exception: #{exceptionMessage} - #{exceptionFilename} #{lineNum}:#{colNum}")
     exit(100)
   end
 
   @@linker.func_new("env", "console.log", [:i32], []) do |_caller, messagePtr|
     message = read_wasm_string(messagePtr)
-    STDOUT.puts(message)
+
+    [messagePtr].each { |m|
+      #noinspection RubyClassVariableUsageInspection
+      raw_bytes = @@memory.read(m - 4, 4).bytes.reverse
+      message_len = 0
+      raw_bytes.each { |j|
+        message_len = (message_len << 8) + (j & 0xFF)
+      }
+      length = message_len
+      result = ""
+      i = 0
+      while i < length
+        result += @@memory.read(m + i, 1)
+        i += 2
+      end
+      STDOUT.puts(message)
+    }
   end
 
   @@linker.func_new("env", "seed", [], [:f64]) do |_caller|
@@ -73,24 +90,25 @@ class LocalBucketing
     set_platform_data('{"sdkType": "server", "sdkVersion":"testing","platformVersion":"1.0.0","platform":"Ruby", "hostname":"localhost"}')
     puts('Set platformdata')
     store_config(sdkkey, '{"project":{"settings":{"edgeDB":{"enabled":false},"optIn":{"enabled":true,"title":"Beta Feature Access","description":"Get early access to new features below","imageURL":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR68cgQT_BTgnhWTdfjUXSN8zM9Vpxgq82dhw&usqp=CAU","colors":{"primary":"#0042f9","secondary":"#facc15"}}},"a0_organization":"org_NszUFyWBFy7cr95J","_id":"6216420c2ea68943c8833c09","key":"default"},"environment":{"_id":"6216420c2ea68943c8833c0b","key":"development"},"features":[{"_id":"6216422850294da359385e8b","key":"test","type":"release","variations":[{"variables":[{"_var":"6216422850294da359385e8d","value":true}],"name":"Variation On","key":"variation-on","_id":"6216422850294da359385e8f"},{"variables":[{"_var":"6216422850294da359385e8d","value":false}],"name":"Variation Off","key":"variation-off","_id":"6216422850294da359385e90"}],"configuration":{"_id":"621642332ea68943c8833c4a","targets":[{"distribution":[{"percentage":0.5,"_variation":"6216422850294da359385e8f"},{"percentage":0.5,"_variation":"6216422850294da359385e90"}],"_audience":{"_id":"621642332ea68943c8833c4b","filters":{"operator":"and","filters":[{"values":[],"type":"all","filters":[]}]}},"_id":"621642332ea68943c8833c4d"}],"forcedUsers":{}}}],"variables":[{"_id":"6216422850294da359385e8d","key":"test","type":"Boolean"}],"variableHashes":{"test":2447239932}}')
+    puts('Set config')
   end
 
   def generate_bucketed_config(user)
     sdkkey_addr = malloc_wasm_string(@sdkkey)
     user_addr = malloc_wasm_string(user)
-    @@instance.invoke("generateBucketedConfigForUser", sdkkey_addr, user_addr)
+    config_addr = @@instance.invoke("generateBucketedConfigForUser", sdkkey_addr, user_addr)
+    read_wasm_string(config_addr)
   end
 
   def store_config(sdkkey, config)
     sdkkey_addr = malloc_wasm_string(sdkkey)
     config_addr = malloc_wasm_string(config)
-    @@instance.invoke("generateBucketedConfigForUser", sdkkey_addr, config_addr)
+    @@instance.invoke("setConfigData", sdkkey_addr, config_addr)
   end
 
   def set_platform_data(platformdata)
     platformdata_addr = malloc_wasm_string(platformdata)
     @@instance.invoke("setPlatformData", platformdata_addr)
-    nil
   end
 
   def init_event_queue(options)
@@ -139,6 +157,7 @@ class LocalBucketing
   end
 
   private
+
   # @param [String] string utf8 string to allocate
   # @return [Integer] address to WASM String
   def malloc_wasm_string(string)
@@ -150,21 +169,24 @@ class LocalBucketing
     start_addr = wasm_new.call(byte_len * 2, wasm_object_id)
     i = 0
     while i < byte_len
-      @@memory.write(start_addr + (i * 2), "#{utf8_bytes[i]}")
+      @@memory.write(start_addr + (i * 2), [utf8_bytes[i]].pack('U'))
       i += 1
     end
-
     start_addr
   end
 
   # @param [Integer] address start address of string.
   # @return [String] resulting string
   def read_wasm_string(address)
-    length = @@memory.read(address - 4, 4).unpack("N").first
+    raw_bytes = @@memory.read(address - 4, 4).bytes.reverse
+    len = 0
+    raw_bytes.each { |j|
+      len = (len << 8) + (j & 0xFF)
+    }
     result = ""
     i = 0
-    while i < length
-      result += @@memory.read(address + i)
+    while i < len
+      result += @@memory.read(address + i, 1)
       i += 2
     end
     result
@@ -173,5 +195,9 @@ class LocalBucketing
 end
 
 # end
-#test_config = `{"project":{"settings":{"edgeDB":{"enabled":false},"optIn":{"enabled":true,"title":"Beta Feature Access","description":"Get early access to new features below","imageURL":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR68cgQT_BTgnhWTdfjUXSN8zM9Vpxgq82dhw&usqp=CAU","colors":{"primary":"#0042f9","secondary":"#facc15"}}},"a0_organization":"org_NszUFyWBFy7cr95J","_id":"6216420c2ea68943c8833c09","key":"default"},"environment":{"_id":"6216420c2ea68943c8833c0b","key":"development"},"features":[{"_id":"6216422850294da359385e8b","key":"test","type":"release","variations":[{"variables":[{"_var":"6216422850294da359385e8d","value":true}],"name":"Variation On","key":"variation-on","_id":"6216422850294da359385e8f"},{"variables":[{"_var":"6216422850294da359385e8d","value":false}],"name":"Variation Off","key":"variation-off","_id":"6216422850294da359385e90"}],"configuration":{"_id":"621642332ea68943c8833c4a","targets":[{"distribution":[{"percentage":0.5,"_variation":"6216422850294da359385e8f"},{"percentage":0.5,"_variation":"6216422850294da359385e90"}],"_audience":{"_id":"621642332ea68943c8833c4b","filters":{"operator":"and","filters":[{"values":[],"type":"all","filters":[]}]}},"_id":"621642332ea68943c8833c4d"}],"forcedUsers":{}}}],"variables":[{"_id":"6216422850294da359385e8d","key":"test","type":"Boolean"}],"variableHashes":{"test":2447239932}}`
+# test_config = `{"project":{"settings":{"edgeDB":{"enabled":false},"optIn":{"enabled":true,"title":"Beta Feature Access","description":"Get early access to new features below","imageURL":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR68cgQT_BTgnhWTdfjUXSN8zM9Vpxgq82dhw&usqp=CAU","colors":{"primary":"#0042f9","secondary":"#facc15"}}},"a0_organization":"org_NszUFyWBFy7cr95J","_id":"6216420c2ea68943c8833c09","key":"default"},"environment":{"_id":"6216420c2ea68943c8833c0b","key":"development"},"features":[{"_id":"6216422850294da359385e8b","key":"test","type":"release","variations":[{"variables":[{"_var":"6216422850294da359385e8d","value":true}],"name":"Variation On","key":"variation-on","_id":"6216422850294da359385e8f"},{"variables":[{"_var":"6216422850294da359385e8d","value":false}],"name":"Variation Off","key":"variation-off","_id":"6216422850294da359385e90"}],"configuration":{"_id":"621642332ea68943c8833c4a","targets":[{"distribution":[{"percentage":0.5,"_variation":"6216422850294da359385e8f"},{"percentage":0.5,"_variation":"6216422850294da359385e90"}],"_audience":{"_id":"621642332ea68943c8833c4b","filters":{"operator":"and","filters":[{"values":[],"type":"all","filters":[]}]}},"_id":"621642332ea68943c8833c4d"}],"forcedUsers":{}}}],"variables":[{"_id":"6216422850294da359385e8d","key":"test","type":"Boolean"}],"variableHashes":{"test":2447239932}}`
 localbucketing = LocalBucketing.new("dvc_server_token_hash", {})
+
+puts("config?")
+puts(localbucketing.generate_bucketed_config('{"user_id":"test"}'))
+puts("woo")
