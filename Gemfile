fc608db9c071d6ad41964f57f7a387559146e867
Jamie Sinn <jamie.sinn@taplytics.com>
2023-02-07 12:44:27 -0500

    Add sorbet + wasm memory read write is done.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
 gemspec
+gem 'sorbet', :group => :development
+gem 'sorbet-runtime'
+gem 'tapioca', require: false, :group => :development
 
 group :development, :test do
   gem 'rake', '~> 13.0.1'
--- lib/devcycle-ruby-server-sdk/localbucketing/localbucketing.rb
+++ lib/devcycle-ruby-server-sdk/localbucketing/localbucketing.rb
@@ -56,7 +56,6 @@ class LocalBucketing
     message = read_wasm_string(messagePtr)
 
     [messagePtr].each { |m|
-      #noinspection RubyClassVariableUsageInspection
       raw_bytes = @@memory.read(m - 4, 4).bytes.reverse
       message_len = 0
       raw_bytes.each { |j|
@@ -91,6 +90,7 @@ class LocalBucketing
     puts('Set platformdata')
     store_config(sdkkey, '{"project":{"settings":{"edgeDB":{"enabled":false},"optIn":{"enabled":true,"title":"Beta Feature Access","description":"Get early access to new features below","imageURL":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR68cgQT_BTgnhWTdfjUXSN8zM9Vpxgq82dhw&usqp=CAU","colors":{"primary":"#0042f9","secondary":"#facc15"}}},"a0_organization":"org_NszUFyWBFy7cr95J","_id":"6216420c2ea68943c8833c09","key":"default"},"environment":{"_id":"6216420c2ea68943c8833c0b","key":"development"},"features":[{"_id":"6216422850294da359385e8b","key":"test","type":"release","variations":[{"variables":[{"_var":"6216422850294da359385e8d","value":true}],"name":"Variation On","key":"variation-on","_id":"6216422850294da359385e8f"},{"variables":[{"_var":"6216422850294da359385e8d","value":false}],"name":"Variation Off","key":"variation-off","_id":"6216422850294da359385e90"}],"configuration":{"_id":"621642332ea68943c8833c4a","targets":[{"distribution":[{"percentage":0.5,"_variation":"6216422850294da359385e8f"},{"percentage":0.5,"_variation":"6216422850294da359385e90"}],"_audience":{"_id":"621642332ea68943c8833c4b","filters":{"operator":"and","filters":[{"values":[],"type":"all","filters":[]}]}},"_id":"621642332ea68943c8833c4d"}],"forcedUsers":{}}}],"variables":[{"_id":"6216422850294da359385e8d","key":"test","type":"Boolean"}],"variableHashes":{"test":2447239932}}')
     puts('Set config')
+    init_event_queue('{"flushEventsMS":1000,"disableAutomaticEventLogging":false,"disableCustomEventLogging":false}')
   end
 
   def generate_bucketed_config(user)
@@ -119,14 +119,14 @@ class LocalBucketing
 
   def flush_event_queue
     sdkkey_addr = malloc_wasm_string(@sdkkey)
-    payload_addr = instance.invoke("flushEventQueue", sdkkey_addr)
+    payload_addr = @@instance.invoke("flushEventQueue", sdkkey_addr)
     payload = read_wasm_string(payload_addr)
   end
 
   def check_event_queue_size
     sdkkey_addr = malloc_wasm_string(@sdkkey)
-    size = instance.invoke("eventQueueSize", sdkkey_addr)
-    size.to_i
+    size = @@instance.invoke("eventQueueSize", sdkkey_addr)
+    size
   end
 
   def on_payload_success(payload_id)
@@ -200,4 +200,5 @@ localbucketing = LocalBucketing.new("dvc_server_token_hash", {})
 
 puts("config?")
 puts(localbucketing.generate_bucketed_config('{"user_id":"test"}'))
-puts("woo")
+puts("event queue size")
+puts(localbucketing.check_event_queue_size)
