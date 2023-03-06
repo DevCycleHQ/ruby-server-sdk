c5a6c6d61b801caf3217c66a79c9de65c655969a
Nika Salamadze <nika.salamadze@taplytics.com>
2023-03-06 11:23:06 -0500

    fix: add config_cdn_uri param to DVCOptions initialize method

--- lib/devcycle-ruby-server-sdk/localbucketing/dvc_options.rb
+++ lib/devcycle-ruby-server-sdk/localbucketing/dvc_options.rb
@@ -12,15 +12,16 @@ module DevCycle
 
     def initialize(
       enable_cloud_bucketing: false,
-      event_flush_interval_ms: 10 * 1000,
+      event_flush_interval_ms: 10_000,
       disable_custom_event_logging: false,
       disable_automatic_event_logging: false,
-      config_polling_interval_ms: 10000,
-      request_timeout_ms: 5000,
-      max_event_queue_size: 2000,
-      flush_event_queue_size: 1000,
+      config_polling_interval_ms: 10_000,
+      request_timeout_ms: 5_000,
+      max_event_queue_size: 2_000,
+      flush_event_queue_size: 1_000,
       event_request_chunk_size: 100,
       logger: nil,
+      config_cdn_uri: 'https://config-cdn.devcycle.com',
       events_api_uri: 'https://events.devcycle.com',
       enable_edge_db: false
     )
@@ -34,8 +35,7 @@ module DevCycle
       end
 
       if config_polling_interval_ms < 1000
-        @logger.warn('config_polling_interval cannot be less than 1000ms, defaulting to 10000ms')
-        config_polling_interval_ms = 10000
+        raise ArgumentError.new('config_polling_interval cannot be less than 1000ms')
       end
       @config_polling_interval_ms = config_polling_interval_ms
 
@@ -51,15 +51,21 @@ module DevCycle
       @event_flush_interval_ms = event_flush_interval_ms
 
       if flush_event_queue_size >= max_event_queue_size
-        raise ArgumentError.new("flush_event_queue_size: #{flush_event_queue_size} must be " +
-          "smaller than max_event_queue_size: #{@max_event_queue_size}")
+        raise ArgumentError.new(
+          "flush_event_queue_size: #{flush_event_queue_size} must be " +
+          "smaller than max_event_queue_size: #{@max_event_queue_size}"
+        )
       elsif flush_event_queue_size < event_request_chunk_size || max_event_queue_size < event_request_chunk_size
-        throw ArgumentError.new("flush_event_queue_size: #{flush_event_queue_size} and " +
+        throw ArgumentError.new(
+          "flush_event_queue_size: #{flush_event_queue_size} and " +
           "max_event_queue_size: #{max_event_queue_size} " +
-          "must be larger than event_request_chunk_size: #{event_request_chunk_size}")
+          "must be larger than event_request_chunk_size: #{event_request_chunk_size}"
+        )
       elsif flush_event_queue_size > 20000 || max_event_queue_size > 20000
-        raise ArgumentError.new("flush_event_queue_size: #{flush_event_queue_size} or " +
-          "max_event_queue_size: #{max_event_queue_size} must be smaller than 20,000")
+        raise ArgumentError.new(
+          "flush_event_queue_size: #{flush_event_queue_size} or " +
+          "max_event_queue_size: #{max_event_queue_size} must be smaller than 20,000"
+        )
       end
       @flush_event_queue_size = flush_event_queue_size
       @max_event_queue_size = max_event_queue_size
@@ -67,7 +73,7 @@ module DevCycle
 
       @disable_custom_event_logging = disable_custom_event_logging
       @disable_automatic_event_logging = disable_automatic_event_logging
-      @config_cdn_uri = "https://config-cdn.devcycle.com"
+      @config_cdn_uri = config_cdn_uri
       @events_api_uri = events_api_uri
       @bucketing_api_uri = "https://bucketing-api.devcyle.com"
     end
