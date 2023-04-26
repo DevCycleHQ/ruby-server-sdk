f63fd1b835fde55dd681762ca619f7e0f0ad1586
Parth Suthar <parth.suthar@taplytics.com>
2023-04-26 11:32:16 -0400

    [chore] update docker setup for better build

--- examples/local/app/controllers/demo_controller.rb
+++ examples/local/app/controllers/demo_controller.rb
@@ -1,17 +1,17 @@
 class DemoController < ApplicationController
   def index
-    # user = DevCycle::UserData.new({ user_id: 'test', country: 'JP' })
-    Rails.logger.info('doing stuff, beep boop')
-    # @bool_var = DevCycleClient.variable(user, 'bool-var', false)
-    # @string_var = DevCycleClient.variable(user, 'string-var', 'default')
-    # @number_var = DevCycleClient.variable(user, 'number-var', 0)
-    # @json_var = DevCycleClient.variable(user, 'json-var-ruby-too', {})
-    #
-    # @non_existent_var = DevCycleClient.variable(user, 'non-existent-variable', "I don't exist")
-    #
-    # @all_variables = DevCycleClient.all_variables(user)
-    #
-    # @all_features = DevCycleClient.all_features(user)
+    user = DevCycle::UserData.new({ user_id: 'test', country: 'JP' })
+    # Rails.logger.info('doing stuff, beep boop')
+    @bool_var = $DevCycleClient.variable(user, 'boolean-variable', false)
+    @string_var = $DevCycleClient.variable(user, 'string-variable', 'default')
+    @number_var = $DevCycleClient.variable(user, 'integer-variable', 0)
+    @json_var = $DevCycleClient.variable(user, 'json-object-variable', {})
+    
+    @non_existent_var = $DevCycleClient.variable(user, 'non-existent-variable', "I don't exist")
+    
+    @all_variables = $DevCycleClient.all_variables(user)
+    
+    @all_features = $DevCycleClient.all_features(user)
   end
 
   def track
@@ -22,18 +22,18 @@ class DemoController < ApplicationController
         country: 'JP'
       })
     event = DevCycle::Event.new({ :'type' => :'randomEval', :'target' => :'custom target' })
-    DevCycleClient.track(user, event)
+    $DevCycleClient.track(user, event)
     render json: "track called on DVC client"
   end
 
   def flush_events
-    DevCycleClient.flush_events
+    $DevCycleClient.flush_events
     render json: "flush_events called on DVC client"
   end
 
   def variable
     user = DevCycle::UserData.new({ user_id: 'test' })
-    variable = DevCycleClient.variable(user, 'v-key-25', false)
+    variable = $DevCycleClient.variable(user, 'v-key-25', false)
     render json: variable
   end
 end
--- examples/local/config/unicorn.rb
+++ examples/local/config/unicorn.rb
@@ -1,7 +1,9 @@
-worker_processes Integer(ENV['WEB_CONCURRENCY'] || 1)
+require "devcycle-ruby-server-sdk"
+worker_processes 4
 timeout 5
 preload_app true
 listen ENV['PORT'] || 3000, tcp_nopush: true
+$DevCycleClient = nil
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
@@ -11,6 +13,7 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
+  $DevCycleClient = DevCycle::DVCClient.new(ENV['DVC_SERVER_SDK_KEY'], DevCycle::DVCOptions.new(enable_cloud_bucketing: false, event_flush_interval_ms: 1000, config_polling_interval_ms: 1000), true)
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
